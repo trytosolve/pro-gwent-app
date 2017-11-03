cd141b9fd9e581d10ff51a27d6b6b90c09015226
trytosolve <trytosolve@gmail.com>
2017-11-03 12:32:09 +0200

    first commit

---
+++ src/main/java/com/iredko/gwent/MainPageController.java
@@ -0,0 +1,19 @@
+package com.iredko.gwent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping(path = "/main")
+public class MainPageController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ModelAndView getMain(ModelAndView model) {
+        model.addObject("text", "Добро пожаловать!");
+        model.setViewName("mainPage");
+        return model;
+    }
+}
---
+++ web/WEB-INF/jsp/mainPage.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: IRedko
+  Date: 03.11.2017
+  Time: 11:43
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<H1>MyGwent</H1>
+${text}
+</body>
+</html>
