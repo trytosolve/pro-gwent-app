b3229ea6c67d2719065f178f95b963f4f9805263
trytosolve <trytosolve@gmail.com>
2017-12-06 15:15:56 +0200

    LoginController update

--- src/main/java/com/iredko/gwent/controllers/LoginController.java
+++ src/main/java/com/iredko/gwent/controllers/LoginController.java
@@ -13,7 +13,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpSession;
 
 @Controller
-@RequestMapping("/login")
+@RequestMapping("/loginPage")
 public class LoginController {
 
     private SecurityManager securityManager;
