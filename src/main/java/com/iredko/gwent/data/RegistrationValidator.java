abc993c744f6c550d2343e6b6371faf9dde7cafc
Ilya Redko <trytosolve@gmail.com>
2018-01-11 23:09:23 +0200

    test

--- src/main/java/com/iredko/gwent/data/RegistrationValidator.java
+++ src/main/java/com/iredko/gwent/data/RegistrationValidator.java
@@ -18,17 +18,29 @@ public class RegistrationValidator implements Validator {
     @Override
     public void validate(Object target, Errors errors) {
         RegistrationForm registrationForm = (RegistrationForm) target;
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"userLogin","userLogin.empty","Login must not be empty.");
-        String username = registrationForm.getUserLogin();
-        if ((username.length()) > 16) {
-            errors.rejectValue("userLogin","userLogin.tooLong","Login must not more than 16 characters.");
-        }
+        validateLogin(errors, registrationForm);
+        validatePassword(errors, registrationForm);
+        validateMail(errors);
+
+    }
+
+    public void validateMail(Errors errors) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userEmail", "userEmail.empty", "Email must not be empty.");
+    }
+
+    public void validatePassword(Errors errors, RegistrationForm registrationForm) {
         ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userPassword", "userPassword.empty", "Password must not be empty.");
         if (!(registrationForm.getUserPassword()).equals(registrationForm
                 .getConfirmPassword())) {
             errors.rejectValue("confirmPassword", "confirmPassword.passwordDontMatch", "Passwords don't match.");
         }
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userEmail", "userEmail.empty", "Email must not be empty.");
+    }
 
+    private void validateLogin(Errors errors, RegistrationForm registrationForm) {
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors,"userLogin","userLogin.empty","Login must not be empty.");
+        String username = registrationForm.getUserLogin();
+        if ((username.length()) > 16) {
+            errors.rejectValue("userLogin","userLogin.tooLong","Login must not more than 16 characters.");
+        }
     }
 }
